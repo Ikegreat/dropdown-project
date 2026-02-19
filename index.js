eebbce3928d9ace0d3bb6c1060684eec91401ca6
unknown <ikechukwuchiemerie822@gmail.com>
2026-02-19 22:18:20 +0100

    desktop javascript functionality added

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const featureHead = document.querySelector("#mobile-feature-head");
 const dropDown = document.querySelector("#mobile-feature-dropdown")
 const companyHead = document.querySelector("#mobile-company-header")
 const companyDropdown = document.querySelector("#mobile-company-dropdown")
-
+const featureHeadDesktop = document.querySelector("#feature-head");
+const featureDropdown = document.querySelector("#feature-dropdown")
+const companyHeadDesktop = document.querySelector("#company-head");
+const companyDropdownDesktop = document.querySelector("#company-dropdown")
 openBtn.addEventListener("click", () => {
     parentMenu.classList.add("active");
     // openBtn.style.display = "none";
@@ -25,3 +28,13 @@ companyHead.addEventListener("click", () => {
     companyDropdown.style.display = 
     companyDropdown.style.display === "block" ? "none" : "block"
 }) 
+featureHeadDesktop.addEventListener("click", ()=> {
+    featureDropdown.style.display =
+    featureDropdown.style.display === "block" ? "none" :"block"
+    
+})
+
+companyHeadDesktop.addEventListener("click", () => {
+    companyDropdownDesktop.style.display =
+    companyDropdownDesktop.style.display === "block" ? "none" : "block"
+})
